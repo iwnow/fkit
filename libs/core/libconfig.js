ab82cc0f554f94da3687f88e26fcdda100e1863f
stacktracer <vostroknutov@hotmail.com>
2019-11-29 18:03:11 +0300

    chore: test config

---
+++ libs/core/libconfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+    build: {
+        name: 'fkit.core',
+        entryFile: 'index.ts',
+        outputFile: 'core',
+        revision: 'time', //time || hash core.23df2g34s4.js || core.1207191723.js
+        lang: 'ts', // ts || js ...
+        tsConfigPath: '',
+    }
+}
